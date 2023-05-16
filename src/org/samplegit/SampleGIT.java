b11c33d0444e6cd7b588594c330ec8a400ff3d2f
Vigneshwaran2020 <varan.rdx@gmail.com>
2023-05-16 13:43:43 +0530

    Edit is done

--- src/org/samplegit/SampleGIT.java
+++ src/org/samplegit/SampleGIT.java
@@ -1,5 +1,11 @@
 package org.samplegit;
 
 public class SampleGIT {
+	public void empId() {
+		System.out.println("Emp Id : 101");
+	}
+	public void empName() {
+		System.out.println("Emp Name : Ram");
+	}
 
 }
